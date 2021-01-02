fcd29f4c2b3f5e665ccfe8f7b5924aee500acc74
Zohaib Hassan <zohaibhassan@Zohaibs-MacBook-Pro.local>
2021-01-02 10:00:00 +0500

    Initial plugin structure

---
+++ advanced-visitor-analytics.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Advanced Visitor Analytics
+ * Description: Tracks live visitors and user engagement metrics.
+ * Version: 1.0.0
+ * Author: Zohaib Hassan
+ */
+
+if (!defined('ABSPATH')) exit;

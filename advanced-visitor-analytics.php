d6e50787a4db685eda4634e1c0e6bb9d4aabef87
Zohaib Hassan <zohaibhassan@Zohaibs-MacBook-Pro.local>
2021-01-18 11:00:00 +0500

    Added admin dashboard page

--- advanced-visitor-analytics.php
+++ advanced-visitor-analytics.php
@@ -12,3 +12,18 @@ function ava_enqueue_scripts() {
     wp_enqueue_script('ava-tracker', plugin_dir_url(__FILE__) . 'assets/js/tracker.js', [], '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'ava_enqueue_scripts');
+
+function ava_admin_menu() {
+    add_menu_page(
+        'Visitor Analytics',
+        'Visitor Analytics',
+        'manage_options',
+        'ava-dashboard',
+        'ava_dashboard_page'
+    );
+}
+add_action('admin_menu', 'ava_admin_menu');
+
+function ava_dashboard_page() {
+    echo "<h1>Visitor Analytics Dashboard</h1>";
+}
